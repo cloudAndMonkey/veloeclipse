a8d39813296d90b3b8feb2e3dae4025aab7f275e
Byron Foster <byron@base2.cc>
2012-10-20 03:31:10 -0700

    Work on issue @11.  Can now drop files, but will still fail in some cases.  Ongoing...

--- src/com/googlecode/veloeclipse/vaulttec/ui/editor/VelocityDocumentProvider.java
+++ src/com/googlecode/veloeclipse/vaulttec/ui/editor/VelocityDocumentProvider.java
@@ -5,6 +5,7 @@ import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
 import org.eclipse.ui.editors.text.FileDocumentProvider;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 
 import com.googlecode.veloeclipse.scanner.VelocityPartitionScanner;
 
@@ -13,13 +14,24 @@ import com.googlecode.veloeclipse.scanner.VelocityPartitionScanner;
  * This class provides the IDocuments used by Velocity editors. These IDocuments
  * have an Velocity-aware partition scanner (multi-line comments) attached.
  */
-public class VelocityDocumentProvider extends FileDocumentProvider
+public class VelocityDocumentProvider extends TextFileDocumentProvider
 {
-
+  @Override
+  public void connect(Object element) throws CoreException {
+    super.connect(element);
+    IDocument document = getDocument(element);
+    if (document != null) {
+      if (document.getDocumentPartitioner() == null) {
+        IDocumentPartitioner partitioner = new FastPartitioner(new
+VelocityPartitionScanner(), VelocityPartitionScanner.TYPES);
+        partitioner.connect(document);
+        document.setDocumentPartitioner(partitioner);
+      }
+    }
+  }
     /*
      * (non-Javadoc)
      * @see org.eclipse.ui.texteditor.AbstractDocumentProvider#createDocument(Object)
-     */
     protected IDocument createDocument(Object anElement) throws CoreException
     {
         IDocument document = super.createDocument(anElement);
@@ -31,4 +43,6 @@ public class VelocityDocumentProvider extends FileDocumentProvider
         }
         return document;
     }
+     */
+    
 }
